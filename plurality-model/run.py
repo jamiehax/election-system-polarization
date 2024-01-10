d67602b13bd8afb00dd76b4041216a74bd308e80
Jamie <jamieh028@gmail.com>
2024-01-09 23:35:49 -0500

    updated run.py file to reflect __init__ parameters for ElectionSystem

--- plurality-model/run.py
+++ plurality-model/run.py
@@ -1,9 +1,8 @@
 from model import ElectionSystem
 
-# run model with 100 agents in a 10x10 grid for 100 steps
-model = ElectionSystem(100, 10, 10)
+# ElectionSystem(num_voters, num_candidates, width,  height)
+model = ElectionSystem(self, 100, 4, 100, 100)
 for i in range(100):
     model.step()
-
 
 # server.launch()
