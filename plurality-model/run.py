bd896e3dd4cfe6f9f48c2a26f694d1615d55ca75
Jamie <jamieh028@gmail.com>
2024-01-22 13:15:27 -0500

    save model data to 'model_data.csv' and agent data to 'agent_data.csv'

--- plurality-model/run.py
+++ plurality-model/run.py
@@ -1,34 +1,21 @@
 from model import ElectionSystem
 
-# winner should be 7
-opinions1 = [
-    (0, 3),
-    (1, 3),
-    (1, 4),
-    (2, 4),
-    (3, 2),
-    (3, 1),
-    (4, 1),
-    (0, 4),
-    (4, 0)
-]
-
-# winner should be 8
-opinions2 = [
-    (0, 3),
-    (1, 3),
-    (1, 4),
-    (3, 0),
-    (3, 2),
-    (3, 1),
-    (4, 1),
-    (0, 4),
-    (4, 0)
-]
+"""
+THINGS WE WANT TO BE MODULAR (I.E. WE CAN CONTROL THROUGH PARAMETERS)
+    - number of voters
+    - number of candidates
+    - the size of opinion space
+    - number of opinion dimensions
+    - the election system (plurality, ranked choice, cardinal)
+    - the method for agents to find other agents to interact with
+    - the method for how agents interact with other agents
+"""
 
 # ElectionSystem(num_voters, num_candidates, width,  height)
-model = ElectionSystem(7, 2, x_max=5, y_max=5, opinions=opinions2)
-for i in range(5):
+model = ElectionSystem(100, 5, x_max=5, y_max=5)
+for i in range(100):
     model.step()
 
-# server.launch()
+# save data
+model.datacollector.get_model_vars_dataframe().to_csv('model_data.csv')
+model.datacollector.get_agent_vars_dataframe().to_csv('agent_data.csv')
